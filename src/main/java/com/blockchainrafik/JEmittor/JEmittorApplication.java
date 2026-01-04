18eb5fa47af31ddb10943878f5cd011c9656f2d2
Biokes <106444765+Biokes@users.noreply.github.com>
2026-01-04 22:28:36 +0100

    update(setup): adde project structure and deps

---
+++ src/main/java/com/blockchainrafik/JEmittor/JEmittorApplication.java
@@ -0,0 +1,13 @@
+package com.blockchainrafik.JEmittor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JEmittorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JEmittorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blockchainrafik/JEmittor/JEmittorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blockchainrafik.JEmittor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JEmittorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
