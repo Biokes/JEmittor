2ab8d45259fd6c38dad12cf46e125df78556f7bd
Biokes <106444765+Biokes@users.noreply.github.com>
2026-01-08 13:41:22 +0100

    update(Auths): added security bean

---
+++ auths/src/main/java/com/jemittor/auths/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.jemittor.auths.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static com.jemittor.auths.utils.Commons.AUTH_ENDPOINT;
+
+@Configuration
+public class SecurityConfig  {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        return http.authorizeHttpRequests(request -> request
+                        .requestMatchers(AUTH_ENDPOINT)
+                        .permitAll()
+                        .anyRequest()
+                        .authenticated())
+                .oauth2Login(Customizer.withDefaults())
+                .build();
+    }
+}
